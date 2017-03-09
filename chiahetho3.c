2c19bfd22196f708470378e64ade46f647f0f1ad
Hoang Nguyen <Hoangnguyenatn@gmail.com>
2017-03-09 18:59:55 +0700

    Create chiahetho3.c

---
+++ chiahetho3.c
@@ -0,0 +1,15 @@
+#include"stdio.h"
+int main()
+{
+	int n,i1,i2,i3;
+	printf("nhap vao so muon kiem tra:");
+	scanf("%d",&n);
+	i1=n/100;
+	i2=(n/10)%10;
+	i3=n%10;
+	
+	if((i1+i2+i3)%3==0)
+	printf("\n%d la so chia het cho 3!",n);
+	else
+	printf("\n%d la so khong chia het cho 3!",n);
+}
