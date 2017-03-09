9b588750ba656cab8c446df1a524f42debaba2f1
Hoang Nguyen <Hoangnguyenatn@gmail.com>
2017-03-09 19:02:29 +0700

    Update chiahetho3.c

--- chiahetho3.c
+++ chiahetho3.c
@@ -13,3 +13,19 @@ int main()
 	else
 	printf("\n%d la so khong chia het cho 3!",n);
 }
+
+
+//
+#include"stdio.h"
+int main()
+{
+	int n,i;
+	printf("nhap vao so muon kiem tra:");
+	scanf("%d",&n);
+	i=n/100+(n/10)%10+n%10;
+	
+	if(i%3==0)
+	printf("\n%d la so chia het cho 3!",n);
+	else
+	printf("\n%d la so khong chia het cho 3!",n);
+}
