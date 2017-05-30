9ccff35b6fbaf6003c8cf88d42229844d0a56f0e
Hoang Nguyen <Hoangnguyenatn@gmail.com>
2017-05-30 09:19:03 +0700

    nhập số âm nếu sai , nhập lại !

---
+++ nhapsoam.c
@@ -0,0 +1,15 @@
+#include"stdio.h"
+#include"conio.h"
+int main()
+{
+	int a,b,d;
+	printf("nhap vao mot so:");
+	scanf("%d",&a);
+while(a>0)
+{
+	printf("nhap lai de:");
+ 	
+	 scanf("%d",&a);
+	 }
+	 printf("so ban vua nhap:%d",a);
+	 }
